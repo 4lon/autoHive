a0adb40e68c59c6ddb85ab568c465505669cf21b
Alon Nusem <61749872+4lon@users.noreply.github.com>
2021-01-29 16:31:29 +1000

    Inventor instruction generation
    To work on this project I needed to generate a large text file with all of inventors instructions. You can run generate_command_file in main.py to see it yourself.

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
 # autoHive
  A python script to automatically make 3D models compatible with the Hive Evo storage system
 
-# Steps
-## 1. Installation
+## Steps
+### 0. Purchase
+This project is built of Daniel O'Connell's Hive Evo design, this script will not work without the relevant cad files when you purchase his design: https://cults3d.com/en/3d-model/home/the-hive-evo
+
+### 1. Installation
 Run this script to install the necessary dependency:
     pip install --upgrade pywin32==225
 
 Note: I'm not sure why it has to be this version, it's possible it would work with others however mine would not
 
-# Credits
-## u/dable82 
+## Credits
+### u/dable82 
 For the basic Autodesk Inventor API script that started this project:
 https://www.reddit.com/r/InventorAPI/comments/b6ihwq/using_python_with_inventor_api/
 
+### Mod the Machine
+A lot of the Inventor API resources I used are from this website. Specifically this page https://modthemachine.typepad.com/my_weblog/2009/03/running-commands-using-the-api.html although written for VBA I managed to translate it into Python and get some of these features working
--- main.py
+++ main.py
@@ -1,7 +1,78 @@
 from win32com.client import Dispatch, GetActiveObject, gencache, constants
 
 
+def generate_command_file(invApp):
+    ControlDefs = invApp.CommandManager.ControlDefinitions
+    commandFile = open(r"commands.txt", "w")
+
+    for command in ControlDefs:
+        instructions = command.InternalName
+        if (command.DescriptionText != ""):
+            instructions += " -\n" + command.DescriptionText + "\n"
+        else:
+            instructions += "\n\n"
+        commandFile.write(instructions)
+    commandFile.close()
+
+
 def main():
+    # Create a new part
+    invDoc = invApp.Documents.Add(constants.kPartDocumentObject, "", True)
+
+    # Casting Document to PartDocument
+    # invPartDoc = mod.PartDocument(invDoc)
+
+    # compdef = invPartDoc.ComponentDefinition
+
+    # # Create a sketch
+    # xyPlane = compdef.WorkPlanes.Item(3)
+    # sketch = compdef.Sketches.Add(xyPlane)
+    #
+    # # Set Geometry variables
+    # tg = invApp.TransientGeometry
+    # lines = sketch.SketchLines
+    #
+    # # Draw Triangle
+    # line1 = lines.AddByTwoPoints(tg.CreatePoint2d(0, 0), tg.CreatePoint2d(4, 0))
+    # line2 = lines.AddByTwoPoints(line1.EndSketchPoint, tg.CreatePoint2d(4, 3))
+    # line3 = lines.AddByTwoPoints(line2.EndSketchPoint, line1.StartSketchPoint)
+    #
+    # # Draw slotted hole
+    # # Create the sketch points.
+    # points = sketch.SketchPoints
+    # arcs = sketch.SketchArcs
+    # pointArray = []
+    # pointArray.append(points.Add(tg.CreatePoint2d(0, 1), False))
+    # pointArray.append(points.Add(tg.CreatePoint2d(0, 0), False))
+    # pointArray.append(points.Add(tg.CreatePoint2d(0, -1), False))
+    # pointArray.append(points.Add(tg.CreatePoint2d(4, -1), False))
+    # pointArray.append(points.Add(tg.CreatePoint2d(4, 0), False))
+    # pointArray.append(points.Add(tg.CreatePoint2d(4, 1), False))
+    # # Draw the geometry.
+    # arc1 = arcs.AddByCenterStartEndPoint(
+    #     pointArray[1], pointArray[0], pointArray[2])
+    # line1 = lines.AddByTwoPoints(pointArray[2], pointArray[3])
+    # arc2 = arcs.AddByCenterStartEndPoint(
+    #     pointArray[4], pointArray[3], pointArray[5])
+    # line2 = lines.AddByTwoPoints(pointArray[5], pointArray[0])
+    #
+    # # Draw Rectangle
+    # rectangle = lines.AddAsTwoPointRectangle(
+    #     tg.CreatePoint2d(0, 0), tg.CreatePoint2d(4, 3))
+    #
+    # # Extrude
+    # profile = sketch.Profiles.AddForSolid()
+    # extrudeDef = compdef.Features.ExtrudeFeatures.CreateExtrudeDefinition(
+    #     profile, constants.kJoinOperation)
+    # extrudeDef.SetDistanceExtent(1, constants.kNegativeExtentDirection)
+    # extrude = compdef.Features.ExtrudeFeatures.Add(extrudeDef)
+
+    # Close Document and Inventor
+    # invPartDoc.Close(SkipSave=True)
+    # invApp.Quit()
+
+
+if __name__ == '__main__':
     try:
         invApp = GetActiveObject('Inventor.Application')
     except:
@@ -12,61 +83,4 @@ def main():
     invApp = mod.Application(invApp)
     # invApp.SilentOperation = True
 
-    # Create a new part
-    invDoc = invApp.Documents.Add(constants.kPartDocumentObject, "", True)
-
-    # Casting Document to PartDocument
-    invPartDoc = mod.PartDocument(invDoc)
-
-    compdef = invPartDoc.ComponentDefinition
-
-    # Create a sketch
-    xyPlane = compdef.WorkPlanes.Item(3)
-    sketch = compdef.Sketches.Add(xyPlane)
-
-    # Set Geometry variables
-    tg = invApp.TransientGeometry
-    lines = sketch.SketchLines
-
-    # Draw Triangle
-    line1 = lines.AddByTwoPoints(tg.CreatePoint2d(0, 0), tg.CreatePoint2d(4, 0))
-    line2 = lines.AddByTwoPoints(line1.EndSketchPoint, tg.CreatePoint2d(4, 3))
-    line3 = lines.AddByTwoPoints(line2.EndSketchPoint, line1.StartSketchPoint)
-
-    # Draw slotted hole
-    # Create the sketch points.
-    points = sketch.SketchPoints
-    arcs = sketch.SketchArcs
-    pointArray = []
-    pointArray.append(points.Add(tg.CreatePoint2d(0, 1), False))
-    pointArray.append(points.Add(tg.CreatePoint2d(0, 0), False))
-    pointArray.append(points.Add(tg.CreatePoint2d(0, -1), False))
-    pointArray.append(points.Add(tg.CreatePoint2d(4, -1), False))
-    pointArray.append(points.Add(tg.CreatePoint2d(4, 0), False))
-    pointArray.append(points.Add(tg.CreatePoint2d(4, 1), False))
-    # Draw the geometry.
-    arc1 = arcs.AddByCenterStartEndPoint(
-        pointArray[1], pointArray[0], pointArray[2])
-    line1 = lines.AddByTwoPoints(pointArray[2], pointArray[3])
-    arc2 = arcs.AddByCenterStartEndPoint(
-        pointArray[4], pointArray[3], pointArray[5])
-    line2 = lines.AddByTwoPoints(pointArray[5], pointArray[0])
-
-    # Draw Rectangle
-    rectangle = lines.AddAsTwoPointRectangle(
-        tg.CreatePoint2d(0, 0), tg.CreatePoint2d(4, 3))
-
-    # Extrude
-    profile = sketch.Profiles.AddForSolid()
-    extrudeDef = compdef.Features.ExtrudeFeatures.CreateExtrudeDefinition(
-        profile, constants.kJoinOperation)
-    extrudeDef.SetDistanceExtent(1, constants.kNegativeExtentDirection)
-    extrude = compdef.Features.ExtrudeFeatures.Add(extrudeDef)
-
-    # Close Document and Inventor
-    # invPartDoc.Close(SkipSave=True)
-    # invApp.Quit()
-
-
-if __name__ == '__main__':
-    main()
+    main(invApp)
