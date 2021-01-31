66935458438b67d413aa29aea28b1d156d474505
Alon Nusem <61749872+4lon@users.noreply.github.com>
2021-01-31 22:30:40 +1000

    Custom shapes and angles working
    The faces and angles are based on sorting them one after another, however I may need to change this in the long run because it doesn't allow for joints of more than 2 pieces. I'm not sure if I'll want that but it is something to keep in mind.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from os import getcwd
 import sys
+from math import cos, sin, radians
 
 FREECADPATH = 'C:\\Program Files\\FreeCAD 0.18\\bin'  # path to your FreeCAD.so or FreeCAD.dll file
 sys.path.append(FREECADPATH)
@@ -8,27 +9,46 @@ sys.path.append(FREECADPATH)
 def main():
     try:
         import FreeCAD
+        import Import
+        import Mesh
     except ValueError:
         print("FreeCAD not found")
+    else:
+        cwd = getcwd()
+        name = "autoHive"
+        doc = FreeCAD.newDocument(name)
 
-    cwd = getcwd()
-    name = "autoHive"
-    doc = FreeCAD.newDocument(name)
+        # faces = ['male', 'female', 'male', 'female', 'male', 'female', 'male', 'female', 'male', 'female']
+        # angles = [60, -60, -60, -60, -60, 60, -60, -60, -60, -60]
+        faces = ['male', 'male', 'female', 'female']
+        angles = [-120, -60, -120, -60]
 
-    import Import
-    types = ["male", "female", "male", "female", "male", "female", "joint120", "joint120", "joint120", "joint120",
-             "joint120", "joint120"]
+        i = 0
+        angle = 0
+        coords = [0, 0]
+        side_length = (40.5 / (3 ** 0.5)) * 2
 
-    for i in range(0, 12, 1):
-        Import.insert(cwd + "\\models\\" + types[i] + ".stp", name)
-        doc.Objects[i].Label = str(i)
-        doc.Objects[i].Placement = FreeCAD.Placement(FreeCAD.Vector(0, 0, 0), FreeCAD.Rotation(i * 60, 0, 0),
-                                                     FreeCAD.Vector(0, 0, 0))
-        # FreeCAD.Placement.move(doc.Objects[i].Placement, FreeCAD.Vector(2,0,0))
+        for j in range(0, len(faces), 1):
+            Import.insert(cwd + "\\models\\" + faces[int(i / 2)] + ".stp", name)
+            doc.Objects[i].Placement = FreeCAD.Placement(FreeCAD.Vector(coords[0], coords[1], 0),
+                                                         FreeCAD.Rotation(angle, 0, 0),
+                                                         FreeCAD.Vector(0, 0, 0))
+            Import.insert(cwd + "\\models\\" + str(angles[int(i / 2)]) + ".stp", name)
+            doc.Objects[i + 1].Placement = FreeCAD.Placement(FreeCAD.Vector(coords[0], coords[1], 0),
+                                                             FreeCAD.Rotation(angle, 0, 0),
+                                                             FreeCAD.Vector(0, 0, 0))
 
-    import Mesh
-    Mesh.export(doc.Objects, cwd + "\\" + name + ".stl")
-    FreeCAD.closeDocument(name)
+            coords[0] += (side_length * cos(radians(angle + 60))) + (
+                    side_length * cos(radians(angle + angles[int(i / 2)] - 60)))
+            coords[1] += (side_length * sin(radians(angle + 60))) + (
+                    side_length * sin(radians(angle + angles[int(i / 2)] - 60)))
+
+            angle += angles[int(i / 2)]
+
+            i += 2
+
+        Mesh.export(doc.Objects, cwd + "\\" + name + ".stl")
+        FreeCAD.closeDocument(name)
 
 
 if __name__ == '__main__':
