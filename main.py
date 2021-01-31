eaa71bbc77dd7ca0d5a9d362b6cadb6bbeeca982
Alon Nusem <61749872+4lon@users.noreply.github.com>
2021-01-31 17:05:28 +1000

    Fully functional script able to make the original hexagon
    This version is able to recreate the orginal hexagon design from each of the components. Next it needs to be made more robust to create custom shapes with different angles

--- main.py
+++ main.py
@@ -12,8 +12,23 @@ def main():
         print("FreeCAD not found")
 
     cwd = getcwd()
-    Doc = FreeCAD.newDocument("autoHive")
-    Doc.saveAs(cwd + u"\\autoHive.FCStd")
+    name = "autoHive"
+    doc = FreeCAD.newDocument(name)
+
+    import Import
+    types = ["male", "female", "male", "female", "male", "female", "joint120", "joint120", "joint120", "joint120",
+             "joint120", "joint120"]
+
+    for i in range(0, 12, 1):
+        Import.insert(cwd + "\\models\\" + types[i] + ".stp", name)
+        doc.Objects[i].Label = str(i)
+        doc.Objects[i].Placement = FreeCAD.Placement(FreeCAD.Vector(0, 0, 0), FreeCAD.Rotation(i * 60, 0, 0),
+                                                     FreeCAD.Vector(0, 0, 0))
+        # FreeCAD.Placement.move(doc.Objects[i].Placement, FreeCAD.Vector(2,0,0))
+
+    import Mesh
+    Mesh.export(doc.Objects, cwd + "\\" + name + ".stl")
+    FreeCAD.closeDocument(name)
 
 
 if __name__ == '__main__':
