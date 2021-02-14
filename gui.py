a69becf1c123429688744861dab61d40bae40f2d
Alon Nusem <61749872+4lon@users.noreply.github.com>
2021-02-14 20:29:52 +1000

    Created Size class and lattice is essentially complete
    I need to work on a method to link the different highlighted components so the tool can tell which parts of the selection are inside and outside to properly align the joints

--- gui.py
+++ gui.py
@@ -3,33 +3,6 @@ import tkinter as tk
 from parts import Joint
 
 
-def overShape(event):
-    print(event)
-
-
-def makeRectangle(canvas, location, line_length, angle):
-    points = [[location[0], location[1]], [None, None], [None, None], [None, None]]
-    # position = location
-    radius = 10
-    angle -= 30
-    points[0][0] += radius * cos(radians(angle))
-    points[0][1] -= radius * sin(radians(angle))
-    angle += 30
-    points[0][0] += radius / 4 * cos(radians(angle))
-    points[0][1] -= radius / 4 * sin(radians(angle))
-    side_length = line_length - (2 * radius * (cos(radians(30)) + 0.25))
-    sides = [side_length, radius] * 2
-    for i in range(1, 4, 1):
-        points[i][0] = points[i - 1][0] + sides[i - 1] * cos(radians(angle))
-        points[i][1] = points[i - 1][1] - sides[i - 1] * sin(radians(angle))
-        angle += 90
-
-    canvas.create_polygon(points[0][0], points[0][1], points[1][0], points[1][1], points[2][0], points[2][1],
-                          points[3][0], points[3][1], fill="#424242", tag='rectangle')
-
-    canvas.tag_bind('rectangle', '<Motion>', overShape)
-
-
 def createLattice(canvas, dim_x, dim_y):
     line_length = min(canvas.winfo_width() / (dim_x - 2), canvas.winfo_height() / ((dim_y - 2)*cos(radians(30))))
     position = [canvas.winfo_width() / 2 - line_length * dim_x / 2, canvas.winfo_height() / 2 - line_length * dim_y / 2]
@@ -39,15 +12,7 @@ def createLattice(canvas, dim_x, dim_y):
         position[0] = start_x + (y % 2 * 0.5) * line_length
         position[1] += line_length * sin(radians(60))
         for x in range(0, dim_x, 1):
-            lattice[x][y] = Joint("j"+str(x)+str(y), position, canvas)
-            lattice[x][y].display()
-            if x != 0:
-                makeRectangle(canvas, position, line_length, 180)
-            if y != 0:
-                if not ((x == 0) and (y % 2 == 0)):
-                    makeRectangle(canvas, position, line_length, 120)
-                if not ((x == dim_x - 1) and (y % 2)):
-                    makeRectangle(canvas, position, line_length, 60)
+            lattice[x][y] = Joint([x,y], line_length, position, canvas)
             position[0] += line_length
 
 
--- parts.py
+++ parts.py
@@ -2,13 +2,19 @@ from math import sin, cos, radians
 
 
 class Part(object):
-    def __init__(self, canvas):
+    def __init__(self, coords, canvas):
         super().__init__()
+        self._coords = coords
         self._active = False
         self._polygon = None
         self._canvas = canvas
-        self._colours = {True: '#B8E2F2',
+        self._colours = {True: '#33D4FF',
                          False: '#424242'}
+        self.display()
+
+    def display(self):
+        self._polygon = self._canvas.create_polygon(self._points, fill=self._colours[self._active], tag=self._id)
+        self._canvas.tag_bind(self._id, '<Button-1>', self.toggle)
 
     def toggle(self, event):
         self._active = not self._active
@@ -19,23 +25,44 @@ class Part(object):
 
 
 class Joint(Part):
-    def __init__(self, id, location, canvas):
-        super().__init__(canvas)
-        self._id = id
-        self._points = []
-        self._radius = 10
+    def __init__(self, coords, line_length, location, canvas):
         self._location = location
+        self._points = []
+        self._radius = 5
+        self._id = "j"+str(coords[0])+str(coords[1])
+        self._sides = {'E': Side(coords, line_length, canvas, location, 0),
+                       'SE': Side(coords, line_length, canvas, location, -60),
+                       'SW': Side(coords, line_length, canvas, location, -120)}
         angle = 30
         for i in range(0, 6, 1):
             self._points.append([self._location[0] + self._radius * cos(radians(angle)),
                                  self._location[1] + self._radius * sin(radians(angle))])
             angle += 60
 
-    def display(self):
-        self._polygon = self._canvas.create_polygon(self._points, fill=self._colours[self._active], tag=self._id)
-        self._canvas.tag_bind(self._id, '<Button-1>', self.toggle)
+        super().__init__(coords, canvas)
 
 
 class Side(Part):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, coords, line_length, canvas, location, angle):
+        self._angle = angle
+
+        points = [[location[0], location[1]], [None, None], [None, None], [None, None]]
+        radius = 5
+        angle -= 30
+        points[0][0] += radius * cos(radians(angle))
+        points[0][1] -= radius * sin(radians(angle))
+        angle += 30
+        points[0][0] += radius / 4 * cos(radians(angle))
+        points[0][1] -= radius / 4 * sin(radians(angle))
+        side_length = line_length - (2 * radius * (cos(radians(30)) + 0.25))
+        sides = [side_length, radius] * 2
+        for i in range(1, 4, 1):
+            points[i][0] = points[i - 1][0] + sides[i - 1] * cos(radians(angle))
+            points[i][1] = points[i - 1][1] - sides[i - 1] * sin(radians(angle))
+            angle += 90
+
+
+        self._id = "s"+str(angle)+str(coords[0])+str(coords[1])
+        self._points = points
+
+        super().__init__(coords, canvas)
