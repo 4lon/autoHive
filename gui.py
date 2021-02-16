6c12c248f5bbead1cc60b2606863739034f31eae
Alon Nusem <61749872+4lon@users.noreply.github.com>
2021-02-16 21:29:08 +1000

    Update gui.py

--- gui.py
+++ gui.py
@@ -4,7 +4,7 @@ from parts import Joint
 
 
 def createLatticeOld(canvas, dim_x, dim_y):
-    line_length = min(canvas.winfo_width() / (dim_x - 2), canvas.winfo_height() / ((dim_y - 2) * cos(radians(30))))
+    line_length = min(canvas.winfo_width() / (dim_x - 2), canvas.winfo_height() / ((dim_y - 2) * sin(radians(60))))
     position = [canvas.winfo_width() / 2 - line_length * dim_x / 2, canvas.winfo_height() / 2 - line_length * dim_y / 2]
     start_x = canvas.winfo_width() / 2 - (line_length * (dim_x - 0.5) / 2)
     lattice = [[None] * dim_y] * dim_x
@@ -16,32 +16,57 @@ def createLatticeOld(canvas, dim_x, dim_y):
             position[0] += line_length
 
 
-def create_lattice(canvas, line_length):
-    width = canvas.winfo_width()
-    height = canvas.winfo_height()
+class Lattice(tk.Canvas):
+    def __init__(self, master=None, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self._line_length = 60
 
-    mid = height / 2
-    offset = 0
+    def create(self):
+        width = self.winfo_width()
+        height = self.winfo_height()
 
-    while offset <= mid:
-        canvas.create_line(0, mid + offset, width, mid + offset, width=3, fill='#424242')
-        canvas.create_line(0, mid - offset, width, mid - offset, width=3, fill='#424242')
-        offset += line_length * cos(radians(30))
+        mid = height / 2
+        offset = 0
 
-    mid = width / 2
-    mid_width = (height / 2) / tan(radians(60))
-    offset = 0
+        while offset <= mid:
+            self.create_line(0, mid + offset, width, mid + offset, width=3, fill='#424242')
+            self.create_line(0, mid - offset, width, mid - offset, width=3, fill='#424242')
+            offset += self._line_length * sin(radians(60))
 
-    while offset <= mid + mid_width:
-        canvas.create_line(mid - mid_width + offset, 0, mid + mid_width + offset, height, width=3, fill='#424242')
-        canvas.create_line(mid - mid_width - offset, 0, mid + mid_width - offset, height, width=3, fill='#424242')
-        canvas.create_line(mid - mid_width + offset, height, mid + mid_width + offset, 0, width=3, fill='#424242')
-        canvas.create_line(mid - mid_width - offset, height, mid + mid_width - offset, 0, width=3, fill='#424242')
-        offset += line_length
+        mid = width / 2
+        mid_width = (height / 2) / tan(radians(60))
+        offset = 0
+
+        while offset <= mid + mid_width:
+            self.create_line(mid - mid_width + offset, 0, mid + mid_width + offset, height, width=3, fill='#424242')
+            self.create_line(mid - mid_width - offset, 0, mid + mid_width - offset, height, width=3, fill='#424242')
+            self.create_line(mid - mid_width + offset, height, mid + mid_width + offset, 0, width=3, fill='#424242')
+            self.create_line(mid - mid_width - offset, height, mid + mid_width - offset, 0, width=3, fill='#424242')
+            offset += self._line_length
 
 
 def find_triangle(event):
-    print(event)
+    w = 500 / 2
+    h = 400 / 2
+    # print(type(event.widget))
+    # event.widget.create_polygon([[0, 0], [0, 100], [100, 0]], fill='#33D4FF')
+    # y = mx + c
+    c = 0
+    # ms = [0, sin(radians(60)), -sin(radians(60))]
+    # for m in ms:
+    #     y = m*(event.x-w) + c
+    # print(y, -(event.y-h))
+    # x = ((event.y - h) - c) / m
+    # side = (event.x - w - x)/abs(event.x - w - x)
+    # print(side)
+    x = event.x - (event.widget.winfo_width() / 2)
+    y = -(event.y - (event.widget.winfo_height() / 2))
+
+    line_length = 60
+    print(y / (line_length * sin(radians(60))))
+    print(x / line_length)
+
+    # event.widget.create_polygon([[line_length, ]])
 
 
 def gui():
@@ -50,14 +75,13 @@ def gui():
 
     w = 500
     h = 400
-    line_length = 60
-    canvas = tk.Canvas(root, width=w, height=h, bg='#121212')
-    canvas.pack()
+    lattice = Lattice(root, width=w, height=h, bg='#121212')
+    lattice.pack()
 
     root.update()
-    create_lattice(canvas, line_length)
+    lattice.create()
 
-    canvas.bind('<Button-1>', find_triangle)
-    canvas.bind('<B1-Motion>', find_triangle)
+    lattice.bind('<Button-1>', find_triangle)
+    lattice.bind('<B1-Motion>', find_triangle)
 
     root.mainloop()
